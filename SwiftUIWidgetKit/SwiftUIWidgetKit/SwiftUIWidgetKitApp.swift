bf96334422e3ae3d67b0a6e8e72c9b9c4359410e
Ahmed Amin <AhmedAminHassanIsmail@gmail.com>
2023-07-25 19:55:54 +0300

    initial xCode IDE and update minimum deployment into iOS 14.0

---
+++ SwiftUIWidgetKit/SwiftUIWidgetKit/SwiftUIWidgetKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWidgetKitApp.swift
+//  SwiftUIWidgetKit
+//
+//  Created by Ahmed Amin on 25/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWidgetKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
